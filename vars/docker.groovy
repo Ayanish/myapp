ae1ccd2f4f64f6ea9920f4adac34e274cd8004f4
simple.sai220@gmail.com <simple.sai220@gmail.com>
2022-03-23 12:33:42 +0530

    docker

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -2,12 +2,7 @@
 
 def call(String tools, String app, String uName) {
   echo "Project Name : ${tools}"
-  sh "docker build \
-  --build-arg maven_version=3.8.4 \
-  --build-arg java_version=11 \
-  -t ${tools} \
-  -f tools.Dockerfile \
-  . "
+  
 
   
   sh "docker build -t ${app} ."
