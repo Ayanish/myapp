83df54b6d1981ed00f799b589f08d568f21d17e3
simple.sai220@gmail.com <simple.sai220@gmail.com>
2022-03-23 09:22:09 +0530

    docker

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String project) {
-  echo "Project Name : ${project}"
-  sh "docker build --buid-arg maven_version=3.8.4 --build-arg java_version=11 -t ${project} ."
+def call(String tools) {
+  echo "Project Name : ${tools}"
+  sh "docker build \
+  --buid-arg maven_version=3.8.4 \
+  --build-arg java_version=11 \
+  -t ${tools} \
+  -f tools.Dockerfile 
+  . "
 }
